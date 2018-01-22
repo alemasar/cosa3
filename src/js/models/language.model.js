40af33652980c616c3e4b4a15ea7ea775c1ea34a
Aleix Masague Comas <alemasar@gmail.com>
2018-01-22 07:14:31 +0100

    Binding the model with the state

--- src/js/models/language.model.js
+++ src/js/models/language.model.js
@@ -9,26 +9,12 @@ function lang(language) {
     this.language = language;
 }
 
-const handler1 = {
-    construct(target, args) {
-        console.log('monster1 constructor called');
-        // expected output: "monster1 constructor called"
-
-        return new target(...args);
-    }
-};
-
-// const proxy1 = new Proxy(monster1, handler1);
-
-//  console.log(new proxy1('fierce').disposition);
-
 const l = new Proxy(lang, {
     construct: function (target, args) {
         console.log(args);
         return new target(...args);
     }
 });
-
 
 export let language = function (args) {
     return new l(args).language;
